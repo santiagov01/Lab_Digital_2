840d26335e7a51a82c444a1115b601647484337d
santiagov01 <santiago.vargas5@udea.edu.co>
2024-03-19 17:53:43 -0500

    Caso especial cero

--- P_2_Sumador/adderunit.sv
+++ P_2_Sumador/adderunit.sv
@@ -13,6 +13,9 @@ module adderunit (dataA, dataB, dataR);
 	logic [25:0] mantissa_sumR = 0; //Resultado de sumar
 	logic [25:0] mantissa_sumR_check = 0;//Actualizar mantisa luego de sumar
 	
+	logic [22:0] special_mantisa = 0;//Mantisa para casos especiales
+	logic [7:0] special_exp = 0;//Exponente en casos especiales
+	logic sign_special = 0; //Signo para casos especiales
 	
 	logic [25:0] mantisa_f_aux; //por si es necesaria 	azando
 	logic [22:0] frac; //para utilizar en el 	azamiento final a izquierda
@@ -23,6 +26,9 @@ module adderunit (dataA, dataB, dataR);
 	logic [1:0] aux_exp;
 	
 	logic [4:0] desplz; //Cantidad de espacios para desplazar mantisa final 
+	
+	logic special_case = 0; //
+	
 	
 //***********************************************
 //				    ADD EXPONENT                    *
@@ -68,18 +74,41 @@ module adderunit (dataA, dataB, dataR);
 		negative_A = (~mantisanorm_A + 1'b1);//A negativo
 		negative_B = (~mantisanorm_B + 1'b1);//B negativo
 		if (dataA[31] ^ dataB[31]) //Si son diferentes los signos
+				begin
 				if (dataA[31])
 				//El ultimo bit corresponde al signo, y los dos siguientes se revisan para ajustar exponente					
 					mantissa_sumR = mantisanorm_B + negative_A;
 				else					
 					mantissa_sumR = mantisanorm_A + negative_B;
+				end
 		else if( dataA[31] & dataB[31]) // si son ambos negativos
 				mantissa_sumR = negative_A + negative_B;
 		else begin //si son ambos positivos
 				mantissa_sumR = mantisanorm_A + mantisanorm_B;
 			end
 	end
-	
+//***********************************************
+//				CASOS ESPECIALES                    *
+//***********************************************	
+always_comb begin
+	special_case = 0;
+	special_mantisa = 0;
+	special_exp = 0;
+	sign_special = 0;
+	if(dataA[31] ^ dataB[31] & dataA[30:0] == dataB[30:0]) begin
+		special_case = 1;
+		special_mantisa = 23'd0;
+		special_exp = 8'd0;
+		sign_special = 0;
+		end
+	else
+		special_case = 0;
+end
+
+//if( Infinito...., - infitino, nan...)
+//if(
+
+
 //***********************************************
 //		Complemento a 2 si es negativo            *
 //***********************************************
@@ -101,7 +130,7 @@ module adderunit (dataA, dataB, dataR);
 		desplz =0 ; 
 		case(lastbits)
 			2'b01: mantisaFinal = mantissa_sumR_check[22:0]; //exponente no cambiaría
-			2'b00:begin // revisar hasta encontrar 1 y desplazar mantisa y restar a exponente -127
+			2'b00:begin // revisar hasta encontrar 1 y desplazar mantisa y restar a exponentes
 					
 					casez(frac)
 						23'b1??????????????????????: desplz =  1;
@@ -145,9 +174,17 @@ module adderunit (dataA, dataB, dataR);
 	
 	// Process: operand validator and result normalizer and assembler
 	always_comb begin
-		dataR[31] = mantissa_sumR[25];//*********
-		dataR[30:23] = exp_sumR_final;
-		dataR[22:0] = mantisaFinal;
+		if(special_case) begin //SI se presentan casos especiales....
+			dataR[31] = sign_special;//Igualar a signo en casos especiales
+			dataR[30:23] = special_exp;
+			dataR[22:0] = special_mantisa;
+			end
+		else begin
+			dataR[31] = mantissa_sumR[25];//*********
+			dataR[30:23] = exp_sumR_final;
+			dataR[22:0] = mantisaFinal;
+			end
+		
 	end
 endmodule
 
@@ -195,6 +232,13 @@ module testbench();
 		dataA = 32'h40E00000;
 		dataB = 32'hC0E00002;
 		#delay;
+		dataA = 32'h00000000;
+		dataB = 32'hC0E00002;
+		#delay;
+		dataA = 32'h40E00002;
+		dataB = 32'hC0E00002;
+		#delay;
+		
 		$stop;
 	end
 	
