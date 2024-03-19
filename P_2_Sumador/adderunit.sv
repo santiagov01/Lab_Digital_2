0fbd3ab327e8280dd2d56c3637dd5a8ad8f5e776
santiagov01 <santiago.vargas5@udea.edu.co>
2024-03-18 20:21:49 -0500

    Continuación sumador. Falta casos especiales

--- P_2_Sumador/adderunit.sv
+++ P_2_Sumador/adderunit.sv
@@ -7,14 +7,22 @@ module adderunit (dataA, dataB, dataR);
 
 	// Internal signals to perform the multiplication
 	logic sign_sumR = 0;
-	logic [7:0] exp_sumR;
+	logic [7:0] exp_sumR, exp_sumR_final;
 	logic [25:0] mantissa_sumR = 0; //****** pendiente modificar el rango
-	
+	logic [25:0] mantissa_sumR_check = 0;
 	logic [25:0] mantisaux_A, mantisaux_B; //*******Falta revisar bien cuantos bits son
 	logic [25:0] mantisanorm_A, mantisanorm_B; //*******Falta revisar bien cuantos bits son
-	logic cout;
 	
+	logic [25:0] mantisa_f_aux; //por si es necesaria 	azando
+	logic [22:0] frac; //para utilizar en el 	azamiento final a izquierda
+	logic [22:0] mantisaFinal;
+	logic cout;
+	logic [1:0] lastbits;
+	logic [22:0] first_one_position;
 	logic [1:0] aux_exp;
+	
+	logic [4:0] desplz; //Cantidad de espacios para desplazar mantisa final 
+	
 //***********************************************
 //				    ADD EXPONENT                    *
 //***********************************************
@@ -45,10 +53,6 @@ module adderunit (dataA, dataB, dataR);
 			mantisanorm_B = mantisaux_B;
 			end
 	end
-//***********************************************
-//				IDENTIFICAR SIGNO                   *
-//***********************************************
-
 
 //***********************************************
 //				Process: mantissa adder             *
@@ -57,6 +61,7 @@ module adderunit (dataA, dataB, dataR);
 	always_comb begin
 		if (dataA[31] ^ dataB[31]) //Si son diferentes los signos
 				if (dataA[31])
+				//El ultimo bit corresponde al signo, y los dos siguientes se revisan para ajustar exponente
 					mantissa_sumR = mantisanorm_B + (~mantisanorm_A + 1'b1);//A negativo
 				else
 					mantissa_sumR = mantisanorm_A + (~mantisanorm_B + 1'b1); //B negativo
@@ -66,15 +71,78 @@ module adderunit (dataA, dataB, dataR);
 			end
 	end
 	
+//***********************************************
+//		Complemento a 2 si es negativo            *
+//***********************************************
+	always_comb begin
+		mantissa_sumR_check = 0;
+		if (mantissa_sumR[25])
+			mantissa_sumR_check = ~mantissa_sumR + 1'b1;
+		else
+			mantissa_sumR_check = mantissa_sumR;			
+	end
+//***********************************************
+//		AJUSTAR MANTISA LUEGO DE SUMA	            *
+//***********************************************
+	always_comb begin
+		first_one_position = 0;
+		lastbits = mantissa_sumR_check[24:23];
+		frac = mantissa_sumR_check[22:0];
+		exp_sumR_final = exp_sumR;
+		mantisaFinal = 0;
+		desplz =0 ; 
+		case(lastbits)
+			2'b01: mantisaFinal = mantissa_sumR_check[22:0]; //exponente no cambiaría
+			2'b00:begin // revisar hasta encontrar 1 y desplazar mantisa y restar a exponente -127
+					
+					casez(frac)
+						23'b1??????????????????????: desplz =  1;
+						23'b01?????????????????????: desplz = 2;
+						23'b001????????????????????: desplz = 3;
+						23'b0001???????????????????: desplz = 4;
+						23'b00001??????????????????: desplz = 5;
+						23'b000001?????????????????: desplz = 6;
+						23'b0000001????????????????: desplz = 7;
+						23'b00000001???????????????: desplz = 8;
+						23'b000000001??????????????: desplz = 9;
+						23'b0000000001?????????????: desplz = 10;
+						23'b00000000001????????????: desplz = 11;
+						23'b000000000001???????????: desplz = 12;
+						23'b0000000000001??????????: desplz = 13;
+						23'b00000000000001?????????: desplz = 14;
+						23'b000000000000001????????: desplz = 15;
+						23'b0000000000000001???????: desplz = 16;
+						23'b00000000000000001??????: desplz = 17;
+						23'b000000000000000001?????: desplz = 18;
+						23'b0000000000000000001????: desplz = 19;
+						23'b00000000000000000001???: desplz = 20;
+						23'b000000000000000000001??: desplz = 21;
+						23'b0000000000000000000001?: desplz = 22;
+						default:  desplz = 23; //falta revisar el caso donde todo es cero, caso especial ?						
+					endcase
+					mantisaFinal = frac << desplz;
+					exp_sumR_final= exp_sumR - 8'b1111_1111 - desplz;
+				end
+				
+				default: 
+					begin
+					mantisaFinal = mantissa_sumR_check[23:1];// 11.xxx ,,, o 10.xxx ,,, => 1.1xxx ó 1.0xxx
+					exp_sumR_final = exp_sumR +1'b1; //Se suma 1 al exponente
+					end
+				 //también serviría el corrimiento a izquierda??
+		endcase
+	end
+
+	
 	// Process: operand validator and result normalizer and assembler
 	always_comb begin
-		dataR[31] = mantissa_sumR[25];//*********
-		if (mantissa_sumR[23]) begin
+		dataR[31] = mantissa_sumR_check[25];//*********
+		if (mantissa_sumR_check[23]) begin
 			dataR[30:23] = exp_sumR + 8'd1;
-			dataR[22:0] = mantissa_sumR[23:1];
+			dataR[22:0] = mantissa_sumR_check[23:1];
 		end else begin
 			dataR[30:23] = exp_sumR;
-			dataR[22:0] = mantissa_sumR[22:0];
+			dataR[22:0] = mantissa_sumR_check[22:0];
 		end
 	end
 endmodule
@@ -86,8 +154,9 @@ module testbench();
 	/* Declaración de señales y variables internas */
 	logic [31:0] dataA, dataB,dataR;
 	logic sign_sumR = 0;
-	logic [7:0] exp_sumR;00000000000000000000000
-	logic [24:0] mantissa_sumR = 0; //****** pendiente modificar el rango
+	logic [7:0] exp_sumR;
+	logic [25:0] mantissa_sumR = 0;
+	logic [25:0] mantissa_sumR_check = 0; //****** pendiente modificar el rango
 	
 	logic [25:0] mantisaux_A, mantisaux_B; //*******Falta revisar bien cuantos bits son
 	logic [25:0] mantisanorm_A, mantisanorm_B; //*******Falta revisar bien cuantos bits son
