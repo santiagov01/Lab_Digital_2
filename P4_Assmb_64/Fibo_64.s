a05251d4efef6d7882e347bb5323a019de518397
santiagov01 <santiago.vargas5@udea.edu.co>
2024-04-19 13:40:27 -0500

    Update Fibo_64.s

--- P4_Assmb_64/Fibo_64.s
+++ P4_Assmb_64/Fibo_64.s
@@ -1,14 +1,17 @@
 .global _start
 _start:
 	.equ MAXN, 50 //Maximo de terminos solicitados
-	.equ M, 90 //Cantidad de terminos a generar
+	.equ M, 5 //Cantidad de terminos a generar
 MOV R0, #M
 LDR R1, =FIBO
 
 BL Fibo
+BL Sort
 
 B Final
 Fibo:
+	SUB SP, SP, #4
+	STR LR, [SP]
 	MOV R2, #0
 	MOV R3, #0
 	STR R2, [R1], #4
@@ -17,19 +20,44 @@ Fibo:
 	ADD R2, R2, #1
 	STR R3, [R1], #4
 	STR R2, [R1], #4 //Guardar segundo termino de serie
-
+	SUB R0, R0, #2
+	//LR
 	//Guardar M, DIR, y el valor anterior en stack inicialmente
 	//M
 	//DIR
 	//0000
 	//0000
-	SUB SP, SP, #16
-	SUB R0, R0, #2
-	STR R3, [SP]
-	STR R3, [SP, #4]
-	STR R1, [SP, #8]
-	STR R0, [SP, #12]
 	
+	/*
+	SUB SP, SP, #20
+	SUB R0, R0, #2
+	STR LR, [SP] //Guardar dirección para que luego regrese
+	STR R3, [SP,#4] //Guardar en stack 0
+	STR R3, [SP, #8]//Guardar en stack 0
+	//Como voy a modificar R0 y R1, stack
+	STR R1, [SP, #12]//Guardo dirreccion en stack
+	STR R0, [SP, #16]//Guardo M en stack
+	*/
+	
+	B LoopFibo
+	
+	MOV PC, LR
+	
+LoopFibo:
+	CMP R0, #0
+	BEQ	Out_Fibo //Si M = 0 termina de generar la serie
+	
+	BL ADD64
+	SUB R0,R0, #1
+	B LoopFibo
+	
+ADD64:
+	MOV PC, LR
+Out_Fibo:
+	LDR LR, [SP]
+	ADD SP, SP, #4
+	MOV PC, LR //Sale de funcion Fibo
+Sort:
 	MOV PC, LR
 Final:
 	B Final
