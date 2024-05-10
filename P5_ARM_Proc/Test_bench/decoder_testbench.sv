fc0b486d59b011929b08515bfc2577be9285db86
unknown <santiago.vargas5@udea.edu.co>
2024-05-10 17:35:58 -0500

    Test bench decoder testing

--- P5_ARM_Proc/Test_bench/decoder_testbench.sv
+++ P5_ARM_Proc/Test_bench/decoder_testbench.sv
@@ -2,27 +2,31 @@
  * Testbench to test the peripherals part
  */ 
 module testbench_decoder();
-    logic [1:0] Op,
-    logic [5:0] Funct,
-	logic [3:0] Rd,
-    logic [1:0] FlagW,
-    logic PCS, RegW, MemW,
-    logic MemtoReg, ALUSrc,
-    logic [1:0] ImmSrc, RegSrc, ALUControleds;
+    logic [1:0] Op;
+    logic [5:0] Funct;
+	logic [3:0] Rd;
+    logic [1:0] FlagW;
+    logic PCS, RegW, MemW;
+    logic MemtoReg, ALUSrc;
+    logic [1:0] ImmSrc, RegSrc, ALUContro;
+	logic NoWrite;
 
 	localparam DELAY = 10;
 	
 	// instantiate device to be tested
-	decoder dut_2(clk, reset, switches, leds);
+	decoder dec(Op, Funct, Rd, FlagW, PCS, RegW, MemW, MemtoReg,
+				ALUSrc, ImmSrc, RegSrc, ALUControl, NoWrite);
 
 	// initialize test
 	initial
 	begin
-		reset <= 0; #DELAY; 
-		reset <= 1; 
-		
-		Op <= 10'd4; #(DELAY*2000);
-		
+		Op <= 2'b00;
+		Funct[4:1] <= 4'b1100; 
+		Funct[0] <= 0;
+		#DELAY
+		Op <= 2'b00;
+		Funct[4:1] <= 4'b1010; 
+		Funct[0] <= 1;
 		$stop;
 	end
 
